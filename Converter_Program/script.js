7384e5ca47e3d637dda7dde44eb4eb18c75cf0ad
andreyvdl <andreyvictordantasleal2016@gmail.com>
2023-02-13 23:26:42 -0300

    kkkkk o cara usa farenraiti kkkkkkk

---
+++ Converter_Program/script.js
@@ -0,0 +1,25 @@
+let miles = 24;
+let kilometers = 40;
+let number = 1.609344;
+let kilometersToMiles = kilometers / number;
+let milesToKilometers = miles * number;
+let celsius = 5;
+let fahrenheit = 41;
+let celsiusToFahrenheit = (celsius * 9 / 5) + 32;
+let fahrenheitToCelsius = (fahrenheit - 32) * 5 / 9;
+
+console.log("Distance conversions");
+console.log("-----------------------");
+console.log(kilometers + " kilometers are:");
+console.log(kilometersToMiles + " miles");
+console.log("----");
+console.log(miles + " miles are:");
+console.log(milesToKilometers + " kilometers");
+console.log("\n");
+console.log("Temperature conversions");
+console.log("-----------------------");
+console.log(fahrenheit + " degrees Fahrenheit are:");
+console.log(fahrenheitToCelsius + " degrees celsius");
+console.log("----");
+console.log(celsius + " degrees celsius are:");
+console.log(celsiusToFahrenheit + " degrees fahrenheit");
