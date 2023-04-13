740ee490886fbfdd60c6482f82c622523f51630b
andreyvdl <andreyvictordantasleal2016@gmail.com>
2023-04-13 20:10:59 -0300

    eu odeio o sistema americano 😡

---
+++ Web_Development/Pounds_to_Ounces_Converter/script.js
@@ -0,0 +1,10 @@
+function poundsToOunces(pounds)
+{
+	const conversion = 16;
+	const output = pounds * conversion;
+
+	return (output + " Ounces");
+}
+
+const result = poundsToOunces(6);
+console.log(result);
